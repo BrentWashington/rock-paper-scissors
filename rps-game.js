7ddd387f0e61cbd9ee8e06a1353def7d6378d88f
Brent Washington <brentwashing@gmail.com>
2021-06-20 16:55:27 -0500

    Add functionality for user to enter their own choice to play the game with

--- rps-game.js
+++ rps-game.js
@@ -4,10 +4,33 @@ game();
 // Carries out a game of five rounds
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerSelection = "rock";
+        let playerSelection = promptPlayer();
         let computerSelection = computerPlay();
         console.log(playRound(playerSelection, computerSelection));
     }
+}
+
+// Receives and returns player's selection
+function promptPlayer() {
+    let playerSelection = prompt("Rock, Paper, or Scissors?");
+
+    // If user input is null or empty, prompt the user again
+    if (playerSelection == null || playerSelection === "") {
+        promptPlayer();
+    }
+
+    // Convert the string to lowercase for easy comparisons
+    playerSelection = playerSelection.toLowerCase()
+
+    /*
+     Ensure that the user entered a valid selection for the game. 
+     Otherwise, prompt them again
+     */
+    if (!(playerSelection === "rock" || playerSelection === "paper" || playerSelection == "scissors")) {
+        promptPlayer();
+    }
+
+    return playerSelection;
 }
 
 // Returns a random selection to play against the player
